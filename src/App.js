609af6b84118679def86f1532077bf1e083e01a8
myungho <skqoaudgh2@hanmail.net>
2020-04-15 17:11:40 +0900

    컴포넌트 분리. 컴포넌트에서 store의 값을 조작하는 방법

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Post from './Post';
 import { connect } from 'react-redux';
 
 class App extends Component {
@@ -9,23 +10,17 @@ class App extends Component {
     return (
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </li>
+          <Post key={post.id} post={post} />
         ))}
       </ul>
     );
   }
 }
 
-// 1. store에 저장된 값을 컴포넌트의 props로 매핑하는 함수
-// ownProps 인수를 통해 함수 내부에서 컴포넌트의 props에 접근할 수 있다.
 const mapStateToProps = (state, ownProps) => {
   return {
     posts: state.posts,
   };
 };
 
-// 2. 매핑 함수를 인수로 하여, react-redux의 connect 함수를 통해 HOC(High-Order Componenet)를 export함
 export default connect(mapStateToProps)(App);
---
+++ src/Post.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Post extends Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+  };
+
+  render() {
+    return (
+      <li key={this.props.post.id}>
+        <h1>{this.props.post.title}</h1>
+        <p>{this.props.post.body}</p>
+        <button onClick={this.handleClick}>Delete</button>
+      </li>
+    );
+  }
+}
+
+// 1. store에 저장된 값을 컴포넌트의 props로 매핑하는 함수
+// ownProps 인수를 통해 함수 내부에서 컴포넌트의 props에 접근할 수 있다.
+const mapStateToProps = (state, ownProps) => {
+  return {
+    posts: state.posts,
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    deletePost: (id) => {
+      dispatch({ type: 'DELETE_POST', id: id });
+    },
+  };
+};
+
+// 3. 매핑 함수를 인수로 하여, react-redux의 connect 함수를 통해 HOC(High-Order Componenet)를 export함
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
