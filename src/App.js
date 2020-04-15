4700c1a5514c1ef3acbef22edbbe6c11d0d5af30
myungho <skqoaudgh2@hanmail.net>
2020-04-15 16:49:58 +0900

    컴포넌트에서 store의 값을 읽는 방법

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import { createStore } from 'redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return <div>Hello World!</div>;
+class App extends Component {
+  render() {
+    // 3. store에 저장된 값이 컴포넌트의 props로 매핑되었으므로 this.props로 사용 가능
+    console.log(this.props);
+    const { posts } = this.props;
+    return (
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
-export default App;
+// 1. store에 저장된 값을 컴포넌트의 props로 매핑하는 함수
+// ownProps 인수를 통해 함수 내부에서 컴포넌트의 props에 접근할 수 있다.
+const mapStateToProps = (state, ownProps) => {
+  return {
+    posts: state.posts,
+  };
+};
+
+// 2. 매핑 함수를 인수로 하여, react-redux의 connect 함수를 통해 HOC(High-Order Componenet)를 export함
+export default connect(mapStateToProps)(App);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,21 @@
 const initState = {
-  posts: [],
+  posts: [
+    {
+      id: '1',
+      title: 'Hello World',
+      body: 'this is Hello world with react redux',
+    },
+    {
+      id: '2',
+      title: 'LARP forever',
+      body: 'GTA:SA SAMP LARP forever! you can`t defeat it',
+    },
+    {
+      id: '3',
+      title: 'EasterEgg',
+      body: 'EasterEgg is found under the sea. what the hell',
+    },
+  ],
 };
 
 const rootReducer = (state = initState, action) => {
